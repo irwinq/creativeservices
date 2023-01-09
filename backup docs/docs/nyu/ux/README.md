ca7480c16d3803d23f6118ff1d76175a5b92c8f3
irwinchyi <wangshuo.qi@nyulangone.org>
2023-01-09 15:45:23 -0500

    updates

--- backup docs/docs/nyu/ux/README.md
+++
@@ -1,6 +0,0 @@
----
-title: User Experience
-lang: en-US
-sidebarDepth: 2
----
-
--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,66 +1,11 @@
 module.exports = {
-    title: 'ITDTP Notes',
-    description: 'ITDTP Notes',
+    title: 'Creative Services',
+    description: 'Creative Services',
     themeConfig: {
         smoothScroll: true,
         lastUpdated: 'Last Updated',
         nav: [
-            { text: 'Motion',
-            items: [
-                {text: 'Cinema 4D', items:[
-                    {text: 'C4D', link: '/Cinema 4D/'},
-                    {text: 'Octane', link: '/Cinema 4D/Octane/'},
-                ]},
-                {text: 'After Effects', items:[
-                    {text: 'AE', link: '/AE/'},
-                    {text: 'Pending', link: '/Cinema 4D/Octane/'},
-                ]},
-            ]  
-        },
-            { text: 'Coding',
-            items: [
-                {text: 'Front-End', items:[
-                    {text: 'p5.js', link: '/Code/Front-End/p5js/'},
-                    {text: 'Vue', link: '/Code/Front-End/vuejs/'},
-                ]},
-                {text: 'Swift UI', items:[
-                    {text: 'Pending', link: '/AE/'},
-                    {text: 'Pending', link: '/Cinema 4D/Octane/'},
-                ]},
-            ]  
-        },
-        { text: '语法',
-            items: [
-                {text: '时态', link: '/Learn/Grammar/Tense/'},
-                {text: 'Pending', link: '/Cinema 4D/Octane/'},
-            ]  
-        },
-        { text: 'PT',
-            items: [
-                {text: 'Personal Training', link: '/Exercise/'},
-                {text: 'CPT', link: '/Exercise/CPT/'},
-                {text: 'Exercise Science', link: '/Exercise/ExerciseScience/'},
-                {text: 'Human Anatomy', link: '/Exercise/Anatomy/'},
-            ]  
-        },
-        { text:'Learn',
-            items: [
-                {text: '2021 Fall', items:[
-                    {text: '3D', link: '/nyu/3D/'},
-                    {text: 'Thesis', link: '/nyu/thesis/'},
-                ]},
-                {text: '2021 Spring', items:[
-                    {text: 'Pre-Thesis', link: '/nyu/prethesis/'},
-                ]},
-                {text: '2020 Fall', items:[
-                    {text: 'Creative Coding', link: '/nyu/creativecoding/'},
-                    {text: 'VR', link:'/nyu/vr/'},
-                    {text: 'UX', link:'/nyu/ux/'},
-                    {text: 'I&P', link:'/nyu/IandP/'}
-                ]},
-            ] 
-        },
-            { text: 'Portfolio', link: 'https://irwinqi.com'},
+            { text: 'Brand Center', link: 'https://brandcenter.med.nyu.edu/'},
         ],
         sidebar: 'auto'
     },
